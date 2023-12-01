7fc61cf5d9224835ccb862d037dc7833b97ef987
Melf Kammholz <melf.kammholz@gmail.com>
2023-12-01 07:07:11 +0100

    day 1

--- day01/A.lua
+++ day01/A.lua
@@ -1,2 +1,15 @@
-print("Hello, world!")
+local sum = 0
+
+for line in io.lines() do
+  -- TODO one match possible?
+  local a, b = line:match('(%d).*(%d)')
+  if a and b then
+    sum = sum + tonumber(a) * 10 + tonumber(b)
+  else
+    a = line:match('%d')
+    sum = sum + tonumber(a) * 10 + tonumber(a)
+  end
+end
+
+print(sum)  -- 55108
 
---
+++ day01/B.lua
@@ -0,0 +1,47 @@
+-- TODO write Tuple class
+local function tuplecmp(l1, l2)
+  local n = math.min(#l1, #l2)
+  for i = 1, n do
+    if l1[i] < l2[i] then
+      return true
+    elseif l1[i] > l2[i] then
+      return false
+    end
+  end
+  return false
+end
+
+
+local nums = {
+  'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'
+}
+
+local sum = 0
+
+for line in io.lines() do
+  local set = {}
+
+  -- check for any digit
+  for i = 1, #line do
+    local val = tonumber(line:sub(i, i))  -- no subscript operator :'(
+    if val then
+      table.insert(set, {i, val})
+    end
+  end
+
+  -- collect all positions of a number
+  for val, num in pairs(nums) do
+    for i = 1, #line do
+      local j = line:find(num, i)
+      if j then
+        table.insert(set, {j, val})
+      end
+    end
+  end
+  table.sort(set, tuplecmp)
+
+  sum = sum + set[1][2] * 10 + set[#set][2]
+end
+
+print(sum)  -- 56324
+
