38caed44ce4cb24a937884f1a9e2012125cc2922
Melf Kammholz <melf.kammholz@gmail.com>
2023-12-02 06:38:24 +0100

    day 2

---
+++ day02/A.lua
@@ -0,0 +1,25 @@
+local lim = {red = 12, green = 13, blue = 14}
+
+local res = 0
+
+for line in io.lines() do
+  -- check maximum draw of each color and set
+  local cnt = {red = 0, green = 0, blue = 0}
+  local id, ss = line:match('Game (%d+): (.+)')
+  for s in ss:gmatch('([^;]+)') do
+    for n, c in s:gmatch('(%d+) ([^,]+)') do
+      cnt[c] = math.max(cnt[c], tonumber(n))
+    end
+  end
+
+  -- check bounds
+  local ok = true
+  for c, m in pairs(lim) do
+    print(c, cnt[c], m)
+    ok = ok and cnt[c] <= m
+  end
+  if ok then res = res + id end
+end
+
+print(res)  -- 2276
+
---
+++ day02/B.lua
@@ -0,0 +1,22 @@
+local res = 0
+
+for line in io.lines() do
+  -- check maximum draw of each color and set
+  local cnt = {red = 0, green = 0, blue = 0}
+  local _, ss = line:match('Game (%d+): (.+)')
+  for s in ss:gmatch('([^;]+)') do
+    for n, c in s:gmatch('(%d+) ([^,]+)') do
+      cnt[c] = math.max(cnt[c], tonumber(n))
+    end
+  end
+
+  -- add power
+  local u = 1
+  for _, m in pairs(cnt) do
+    u = u * m
+  end
+  res = res + u
+end
+
+print(res)  -- 68638
+
