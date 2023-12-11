ef2836b36dd449d8b555ce148d8d486c0e8b47e2
Melf Kammholz <melf.kammholz@gmail.com>
2023-12-11 07:29:40 +0100

    day 11 write B in a functional style

--- day11/B.lua
+++ day11/B.lua
@@ -1,65 +1,61 @@
-require("util")
+local fun = require("fun")
+local util = require("util")
 
 -- read input
 local g = {}
 for r in io.lines() do
-  g[#g + 1] = r
+  g[#g + 1] = util.tocharlist(r)
 end
-
-local rows, cols = {}, {}
 
 -- compute indices of empty rows
-for y = 1, #g do
-  local cont = false
-  for x = 1, #g[y] do
-    cont = cont or g[y][x] == "#"
-  end
-  if not cont then rows[#rows + 1] = y end
-end
+local rows = fun.range(#g)
+  :filter(function(y)
+    return not fun.foldl(function(a, x) return a or x == "#" end, false, g[y])
+  end)
+  :totable()
 
 -- compute indices of empty cols
-for x = 1, #g[1] do
-  local cont = false
-  for y = 1, #g do
-    cont = cont or g[y][x] == "#"
-  end
-  if not cont then cols[#cols + 1] = x end
-end
+local cols = fun.range(#g[1])
+  :filter(function(x)
+    return not fun.range(#g)
+      :foldl(function(a, y) return a or g[y][x] == "#" end, false)
+  end)
+  :totable()
 
 -- compute positions of galaxies
-local pos = {}
-for y = 1, #g do
-  for x = 1, #g[y] do
-    if g[y][x] == "#" then
-      pos[#pos + 1] = {y, x}
-    end
-  end
-end
+local pos = fun.range(#g):foldl(function(a, y)
+  return fun.range(#g[y])
+    :map(function(x)
+      return g[y][x] == "#" and {y, x} or false
+    end)
+    :foldl(function(b, p)
+      if p then
+        b[#b + 1] = p
+      end
+      return b
+    end, a)
+end, {})
 
 -- count empty rows/cols in range [l, r]
 local function inrange(x, l, r)
-  local cnt = 0
   if l > r then l, r = r, l end
   -- linear search instead of binary search :)
-  for i = 1, #x do
-    if l <= x[i] and x[i] <= r then
-      cnt = cnt + 1
-    end
-  end
-  return cnt
+  return fun.range(#x)
+    :filter(function(i) return l <= x[i] and x[i] <= r end)
+    :length()
 end
 
 -- compute distances
-local res = 0
-for i = 1, #pos do
+local res = fun.range(#pos - 1):foldl(function(r1, i)
   local y1, x1 = table.unpack(pos[i])
-  for j = i + 1, #pos do
+  return fun.range(i + 1, #pos):foldl(function(r2, j)
     local y2, x2 = table.unpack(pos[j])
     local s1 = inrange(rows, y1, y2)
     local s2 = inrange(cols, x1, x2)
-    res = res + math.abs(y2 - y1) + math.abs(x2 - x1)
-    res = res + (s1 + s2) * (1000000 - 1)  -- account for double counting -1
-  end
-end
+    r2 = r2 + math.abs(y2 - y1) + math.abs(x2 - x1)
+    r2 = r2 + (s1 + s2) * (1000000 - 1)  -- account for double counting -1
+    return r2
+  end, r1)
+end, 0)
 print(res)  -- 699909023130
 
